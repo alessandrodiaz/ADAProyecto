653ddc829370b095fdd5d0d9bf547ff2c1a0bd85
stiven-acua17 <116978111+stiven-acua17@users.noreply.github.com>
2023-01-28 22:51:00 -0500

    Update punto2.py

--- Proyecto/Punto 2/punto2.py
+++ Proyecto/Punto 2/punto2.py
@@ -1,3 +1,5 @@
+# PUNTO 2
+
 class Nodo:
 
     def __init__ (self, letra):
@@ -6,23 +8,22 @@ class Nodo:
         self.sons = dict()
         self.end = False
 
-
-    #Elegir siguiente
+    
+    # Elije al nodo hijo que mas hijos tiene o, en caso de existir un empate
+    # elige dado el orden lexicografico
     def elegir_siguiente(self):
-
         sons = self.sons
         l_s = list(sons)
-        eleccion = sons[ l_s[0]]
+        eleccion = sons[ l_s[0] ]
 
         for i in range (0, len(l_s)):
-
             if sons[ l_s[i] ].words > eleccion.words:
                 eleccion = sons[ l_s[i] ]
+                
             elif sons[ l_s[i] ].words == eleccion.words:
                 if sons[ l_s[i] ].letra < eleccion.letra:
                     eleccion = sons[ l_s[i] ]
                     
-    
         return eleccion
 
 
@@ -32,84 +33,73 @@ class Trie:
         self.root = Nodo("root")
         
 
-    # Insertar    
+    # Añade una nueva palabra al Trie
+    # Complejidad O(n)
     def insertar (self, palabra):
-
         root = self.root
         
         for letra in palabra:
-            #print(root.letra, root.words)
-            
             if letra not in root.sons:
                 root.sons[ letra ] = Nodo( letra )
             root.words = root.words + 1
-            #print(root.letra, root.words)
             root = root.sons[ letra ]
             
-        #print(root.letra, root.words)
         root.end = True
 
 
-    #Buscar Prefijo
+    # Busca si existe una palabra que contenga un prefijo dado
+    # Complejidad O(n)
     def buscar_prefijo (self, palabra):
-
         root = self.root
         
         for letra in palabra:
-
             if letra not in root.sons:
                 return False
-
             root = root.sons[ letra ]
 
         return True
 
 
-    #Buscar Palabra
+    # Busca si el Trie contiene una palabra
+    # Complejidad O(n)
     def buscar_palabra (self, palabra):
-
         root = self.root
 
         for letra in palabra:
-
             if letra not in root.sons:
                 return False
-
             root = root.sons[ letra ]
-
+            
         return root.end
 
 
-    #Eliminar Palabra
+    # Elimina una palabra de la estructura
+    # Complejidad O(n)
     def eliminar_palabra (self, palabra):
 
-        root = self.root
-
-        for letra in palabra:
-
+        if self.buscar_palabra(palabra):
             root.words = root.words - 1
-            
-            if root.sons[ letra ].words == 0:
-                root.sons[ letra ] = null    
-                break
 
-            root.words = root.words + 1
-            
-            root = root.sons[ letra ]
+            for letra in palabra:
+                root.sons[ letra ].words = root.sons[ letra ].words - 1
+
+                if root.sons[ letra ].words == 0:
+                    root.sons[ letra ] = null    
+                    break
+
+                root = root.sons[ letra ]
+
+        else:        
+            return "La palabra no pertenece a la estructura"
 
 
-
-
-
-
-    #buscarSugerencia
+    # Busca la palabra que representa una mejor opcion para completar un texto dado
+    
     def buscar_sugerencia(self, palabra):
-
         root = self.root
         sugerencia = ""
 
         for letra in palabra:
-            #print(root.letra)
             if letra not in root.sons:
                 break
             
@@ -117,30 +107,30 @@ class Trie:
             root = root.sons[letra]
 
         while root.end != True:
-            
             root = root.elegir_siguiente()
             sugerencia = sugerencia + root.letra
             
-
         return sugerencia
 
 
-    #Leer instrucciones
+    #Leer instrucciones del archivo .in
     def leer_instrucciones(self, string, out):
 
         string = string.split(" ")
 
         query = str(string[0].rstrip())
-        a = str(string[1].rstrip())
+        s = str(string[1].rstrip())
 
         if query == "1":
-            self.insertar(a)
+            self.insertar(s)
         else:
-            out.write(str (self.buscar_sugerencia(a)).lower() + "\n")
-            print (self.buscar_sugerencia(a))
+            out.write(str (self.buscar_sugerencia(s)).lower() + "\n")
+            print(self.buscar_sugerencia(s))
 
 
-#Leer archivo
+
+# Poner nombre del archivo de lectura, sin la extension
+# Toma el archivo .in y escribe en la salida .out
 PrefixTree = Trie()
 
 nombre_archivo = 'P2test2'
@@ -158,4 +148,3 @@ for x in entrada:
 
 entrada.close()
 salida.close()
-
